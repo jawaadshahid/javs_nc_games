aad70e19280a2362c2016e44a4963b8e05a233ce
Jawaad Shahid <jawaad.shahid.gb@informa.com>
2021-11-30 17:40:36 +0000

    setup test script and /api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,19 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("endpoint: '/api'", () => {
+  test("status 200: responds with string 'All OK from API Router'", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ text }) => {
+        expect(text).toBe("All OK from API Router");
+      });
+  });
+});
