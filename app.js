7987c1130e2bc975b17c77514d18f2eda360e461
Jawaad Shahid <jawaad.shahid.gb@informa.com>
2021-12-01 16:00:49 +0000

    adds custom error handler

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { handleServerErrors } = require("./errors");
+const { handleServerErrors, handleCustomErrors } = require("./errors");
 const apiRouter = require("./routers/api.router");
 
 const app = express();
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.use(handleCustomErrors);
 app.use(handleServerErrors);
 
 module.exports = app;
--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,9 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
 exports.handleServerErrors = (err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "Internal Server Error" });
